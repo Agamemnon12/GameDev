d2beebbd7379a5806a87f3beadb8c5ab55035f3d
Archie Petin <GamingMusic559@gmail.com>
2018-12-12 17:42:00 -0500

    ADDED MUSIC TO LEVEL 1
    Need to add reference to Cowboy Bebop and Metroid Prime since I used music from there

--- TeamProj/Assets/Scripts/Archie/LeaderBoard.cs
+++ TeamProj/Assets/Scripts/Archie/LeaderBoard.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+ 
 
 public class LeaderBoard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        ILeaderboard leaderboard = Social.CreateLeaderboard();
+        leaderboard.id = "Leaderboard012";
+        leaderboard.LoadScores(result =>
+        {
+            Debug.Log("Recieved " + leaderboard.scores.Length + " scores");
+            foreach (IScore score in leaderboard.scores)
+                Debug.Log(score);
+        });
 	}
 	
 	// Update is called once per frame
