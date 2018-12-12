e6af28cd11b962bbc330b594a3593ab35425f7c1
Archie Petin <GamingMusic559@gmail.com>
2018-12-11 23:17:09 -0500

    ADDED MUSIC AND LEADERBOARD SCRIPT

---
+++ TeamProj/Assets/Scripts/Archie/Music.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The purpose of this script is trying to make it so that the music for the menu and leaderboard continues for one another
+public class Music : MonoBehaviour {
+
+    private static Music instance = null;
+    public static Music Instance
+    {
+        get { return instance; }
+    }
+
+	void Awake ()
+    {
+		if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+}
+
